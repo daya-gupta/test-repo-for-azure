057ab6d6345a23599d3f20ff47c319dc83426e3a
Dayanand Gupta <dayanandgupta@Dayanands-MacBook-Pro.local>
2020-04-19 10:47:50 +0530

    adding apis

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
-const path = require('path');
-
 const express = require('express');
+const path = require('path');
+const https = require('https');
+// const cors = require('cors');
 
 // Constants
 const PORT = 8080;
@@ -35,6 +36,77 @@ app.get('/index', (req, res) => {
 app.get('/watchlist', (req, res) => {
   res.sendFile('build/watchlist.html', {root: __dirname});
 });
+
+// {"url":"/company/AUBANK/","id":1274092,"name":"AU Small Finance Bank Ltd","checked":false},
+// {"url":"/company/ALKEM/consolidated/","id":1273171,"name":"Alkem Laboratories Ltd"},
+// {"url":"/company/DMART/consolidated/","id":1273670,"name":"Avenue Supermarts Ltd"}
+
+app.get('/consolidatedData', (req, res) => {
+  const url = req.query.url;
+  try {
+    https.get(`https://www.screener.in/${url}`, (resp) => {
+      // res.type('string');
+      let data = '';
+
+      resp.on('data', (chunk) => {
+          data += chunk;
+      });
+
+      resp.on('end', () => {
+          res.send(data);
+      });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+  }
+  catch (e) {
+    res.status(500).send('Invalid request');
+  }
+})
+
+app.get('/searchCompany', (req, res) => {
+  const data = req.query.data;
+  https.get(`https://www.screener.in/api/company/search/?q=${data}`, (resp) => {
+      let data = '';
+
+      resp.on('data', (chunk) => {
+          data += chunk;
+      });
+
+      resp.on('end', () => {
+          res.send(JSON.parse(data));
+      });
+
+  }).on("error", (err) => {
+      console.log("Error: " + err.message);
+  });
+})
+
+
+app.get('/historicalData', (req, res) => {
+  const companyId = req.query.companyId;
+  const duration = req.query.duration || 356;
+  // https.get(`https://www.screener.in/api/2/company/${companyId}/prices/?days=${duration}`, (resp) => {
+  try {
+    https.get(`https://www.screener.in/api/company/${companyId}/chart/?q=Price-Volume&days=${duration}`, (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            res.send(JSON.parse(data));
+        });
+
+        }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+  } catch (e) {
+    res.status(500).send('Invalid request');
+  }
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
