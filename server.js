74e6c9b85b2ed194ea37e6c7b058b50ea09aaff5
Dayanand Gupta <dayanandgupta@Dayanands-MacBook-Pro.local>
2020-04-20 00:31:09 +0530

    introducing mongo

--- server.js
+++ server.js
@@ -12,8 +12,39 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello there 5 !!');
+  // get data from mongo and send along
+  let response = 'hello there 5555 !!';
+  db.collection('watchlist').find({}).toArray((err, data) => {
+    if (err) {
+      response += ' database error!!!'
+      res.send(response);
+    } else {
+      data.forEach(function(d){
+        response += `\n ${d.name} ${d.url} ${d.companyId}`;
+      });
+      res.send('<h1>' + response + '</h1>');
+    }
+  }); 
 });
+
+app.get('/add', (req, res) => {
+  // get data from mongo and send along
+  const record = { name: 'infosys', companyId: '1234', url: '/infy' };
+  db.collection('watchlist').find({}).toArray((err, data) => {
+    if (err) {
+      response += ' database error!!!'
+      res.send(response);
+    } else {
+      const lastId = data.length ? Number(data[data.length - 1].companyId) : 10000;
+      record.companyId = lastId + 1;
+      db.collection('watchlist').insert(record).then(res => {
+        res.send('success');
+      });    
+    }
+  });
+});
+
+
 
 // app.get('/getSampleData', (req, res) => {
 //   // const testObj = {
@@ -106,3 +137,62 @@ app.get('/historicalData', (req, res) => {
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+// Mongoose connection
+const mongoose = require('mongoose');
+
+var watchlistSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  companyId: {
+    type: Number
+  },
+  url: {
+    type: String
+  }
+});
+// Create an instance of model SomeModel
+mongoose.model('Watchlist', watchlistSchema);
+
+// Save the new model instance, passing a callback
+// watchlistModel.save(function (err) {
+//   if (err) console.log('schema error');
+// });
+
+const dbConfig = {
+  "uri": "127.0.0.1",
+  "port": "27017",
+  "options": {
+    "useNewUrlParser": true,
+    "poolSize": 5,
+    "connectTimeoutMS": 1000
+  },
+  "name": "testdb"
+};
+const prodUrl = 'mongodb://test-cosmos-1:bzRmqiwJi3pLAa7b2V4oA9qBuVd8FNwW2FtmWQi5EtISlo1nmxwd5IQAauWtYlCLM5Fs8UHITtjziFYZ2fkmlQ==@test-cosmos-1.mongo.cosmos.azure.com:10255/?ssl=true&appName=@test-cosmos-1@';
+const localUrl = `mongodb://${dbConfig.uri}:${dbConfig.port}/${dbConfig.name}`;
+const url = process.env.environment === 'production' ? prodUrl : localUrl;
+let db = null;
+mongoose.connect(url, dbConfig.options)
+  .then(()=> {
+    console.log(`Connected to mongodb successfully`);
+    db = mongoose.connection;
+})
+  .catch(()=> { console.log(`Error while connecting to ${dbConfig.name}`)});
+
+// mongoose.connect('mongodb://localhost/nodemongo');
+// const db = mongoose.connection;
+
+// Check for DB connection
+// db.once('open', function(){
+//     console.log("Connected to MongoDB successfully!");
+// });
+// db.on('error', function(){
+//     console.log(err);
+// });
+
+// var mongoClient = require("mongodb").MongoClient;
+// mongoClient.connect("mongodb://test-cosmos-1:bzRmqiwJi3pLAa7b2V4oA9qBuVd8FNwW2FtmWQi5EtISlo1nmxwd5IQAauWtYlCLM5Fs8UHITtjziFYZ2fkmlQ==@test-cosmos-1.mongo.cosmos.azure.com:10255/?ssl=true&appName=@test-cosmos-1@", function (err, db) {
+//   db.close();
+// });
