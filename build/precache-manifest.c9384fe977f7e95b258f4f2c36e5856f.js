a056eac95ab5d2df7d8fba391d59da22f4da6b93
Dayanand Gupta <dayanandgupta@Dayanands-MacBook-Pro.local>
2020-04-19 11:25:36 +0530

    adding react-project

---
+++ build/precache-manifest.c9384fe977f7e95b258f4f2c36e5856f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354efefa68a148cc31cd3c2cf04cd9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e972c211938f3ca21ac",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b8a84806850e03124f82",
+    "url": "/static/js/2.004140a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.004140a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e972c211938f3ca21ac",
+    "url": "/static/js/main.47686693.chunk.js"
+  },
+  {
+    "revision": "a0711ac394cad94abecb",
+    "url": "/static/js/runtime-main.bdbd80b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
